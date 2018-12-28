4bf843a66ced136e5b15afd9bcf339f69ee6ca2d
Raphael DDL Oliveira <raphael.s.oliveira@bloomingdales.com>
2018-12-28 17:44:16 -0200

    added hero routes

--- src/server/components/Router.js
+++ src/server/components/Router.js
@@ -57,4 +57,54 @@ router.get(
 	})
 );
 
+//* ------------------------
+// Heroes
+//------------------------ */
+router.get(
+	'/hero/:fileId',
+	asyncRoute(async (req, res, next) => {
+		const collection = Database.getCollection('hero'),
+			{ fileId } = req.params;
+
+		if (!collection || !fileId) {
+			return mountApiErrorResponse(res, MESSAGES.db.dbConnectionQuery);
+		}
+
+		let queryCursor = await collection
+			.find({
+				fileId,
+			})
+			.limit(1);
+
+		return queryCursor.toArray((...args) => mountApiResponse(queryCursor, res, ...args));
+	})
+);
+router.get(
+	'/hero',
+	// `:variable`, `:optionalvariable?`
+	asyncRoute(async (req, res, next) => {
+		const collection = Database.getCollection('hero');
+
+		if (!collection) {
+			return mountApiErrorResponse(res, MESSAGES.db.dbConnectionQuery);
+		}
+
+		// req.params.results resource/{number}
+		// req.query.results resource?results={number}
+
+		let queryCursor;
+
+		// https://docs.mongodb.com/manual/tutorial/project-fields-from-query-results/
+		queryCursor = await collection
+			.find()
+			.project({ _id: 0, name: 1, rarity: 1, element: 1, classType: 1, fileId: 1 })
+			// https://docs.mongodb.com/manual/reference/method/cursor.sort/index.html#sort-asc-desc
+			.sort({
+				rarity: -1,
+			});
+
+		return queryCursor.toArray((...args) => mountApiResponse(queryCursor, res, ...args));
+	})
+);
+
 export default router;
