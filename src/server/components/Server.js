322d2f47b75b445a0c1cf6474a0a76c953fdad3f
Raphael DDL Oliveira <raphael.s.oliveira@bloomingdales.com>
2019-01-02 18:16:33 -0200

    added nocache to api

--- src/server/components/Server.js
+++ src/server/components/Server.js
@@ -20,6 +20,7 @@ export default class Server {
 		this.app.use(bodyParser.json());
 		this.app.use(helmet());
 		this.app.use(helmet.permittedCrossDomainPolicies());
+		this.app.use(helmet.noCache());
 		this.app.use(cors());
 		this.app.options('*', cors());
 		this.app.use(
