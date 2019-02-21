fe0e56e44b74ad6807d6f379dd89ce834d57abdf
Raphael DDL Oliveira <raphael.s.oliveira@bloomingdales.com>
2019-02-21 18:19:36 -0300

    fixing the deletion of wrong router

--- src/server/components/Router.js
+++ src/server/components/Router.js
@@ -83,103 +83,6 @@ router.get(
 //* ------------------------
 // Heroes
 //------------------------ */
-router.get(
-	'/hero/:fileId',
-	asyncRoute(async (req, res, next) => {
-		const collection = Database.getCollection('hero'),
-			{ fileId } = req.params;
-
-		if (!collection || !fileId) {
-			return mountApiErrorResponse(res, MESSAGES.query.invalid);
-		}
-
-		// let queryCursor = await collection
-		// 	.aggregate([
-		// 		{ $match: { fileId } },
-		// 		{
-		// 			$lookup: {
-		// 				from: 'hero',
-		// 				as: 'relationsFull',
-		// 				localField: 'relations.hero',
-		// 				foreignField: 'fileId',
-		// 			},
-		// 		},
-		// 	]);
-
-		//get hero
-		let queryCursor = collection.find({ fileId }).limit(1);
-
-		return await queryCursor
-			.toArray()
-			.then(async (currentHero) => {
-				if (currentHero && currentHero[0]) {
-					currentHero = currentHero[0];
-					let relations = [];
-					currentHero.relations.forEach((relation) => {
-						relations.push(relation.hero);
-					});
-
-					//get data of all heroes on relation
-					let queryCursorForRelationship = collection
-						.find({ fileId: { $in: [...relations] } })
-						.project({ name: 1, fileId: 1, _id: 0 });
-
-					return await queryCursorForRelationship.toArray().then(async (relationHeroes) => {
-						if (relationHeroes && relationHeroes.length) {
-							let newRelationArray = currentHero.relations.map((hero) => {
-								let currSome = 0;
-								if (
-									relationHeroes.some((e, i) => {
-										currSome = i;
-										return e.fileId === hero.hero;
-									})
-								) {
-									return { ...hero, ...relationHeroes[currSome] };
-								}
-							});
-							//clean the array entries of heroes I don't have data on
-							currentHero.relations = newRelationArray.filter((e) => {
-								if (e && e.name) {
-									//already have the name and fileId
-									delete e.hero;
-								}
-								return e !== undefined;
-							});
-						} else {
-							//clean relations as we have zero heroes on relations
-							currentHero.relations = [];
-						}
-
-						//no specialty, end
-						if (!currentHero.specialtyChangeName) {
-							return mountApiResponse({}, res, null, [currentHero]);
-						}
-
-						let queryCursorForSpecialty = collection
-							.find({ fileId: currentHero.specialtyChangeName })
-							.project({ _id: 0, name: 1 })
-							.limit(1);
-
-						return await queryCursorForSpecialty.toArray().then((specialtyHeros) => {
-							if (!specialtyHeros || !specialtyHeros.length) {
-								return mountApiResponse({}, res, null, [currentHero]);
-							}
-							currentHero.specialtyChangeName = {
-								fileId: currentHero.specialtyChangeName,
-								name: specialtyHeros[0].name,
-							};
-							return mountApiResponse({}, res, null, [currentHero]);
-						});
-					});
-				} else {
-					return new Promise.reject();
-				}
-			})
-			.catch(() => {
-				return mountApiErrorResponse(res, MESSAGES.query.invalid);
-			});
-	})
-);
 
 router.get(
 	'/hero/:_id',
@@ -330,6 +233,87 @@ router.get(
 		// 	.catch(() => {
 		// 		return mountApiErrorResponse(res, MESSAGES.query.invalid);
 		// 	});
+	})
+);
+
+router.get(
+	'/hero',
+	// `:variable`, `:optionalvariable?`
+	asyncRoute(async (req, res, next) => {
+		const collection = Database.getCollection('hero');
+
+		if (!collection) {
+			return mountApiErrorResponse(res, MESSAGES.db.dbConnectionQuery);
+		}
+
+		let queryCursorForHeroList = collection
+			.aggregate([
+				{
+					$project: {
+						_id: 0,
+						fileId: 1,
+						name: 1,
+						rarity: 1,
+						element: 1,
+						classType: 1,
+						zodiac: 1,
+						skills: 1,
+					},
+				},
+				{
+					$addFields: {
+						buffs: {
+							$map: {
+								input: '$skills.buffs',
+								as: 'el',
+								in: '$$el',
+							},
+						},
+						debuffs: {
+							$map: {
+								input: '$skills.debuffs',
+								as: 'el',
+								in: '$$el',
+							},
+						},
+					},
+				},
+				{
+					$project: {
+						skills: 0,
+					},
+				},
+
+				// {
+				//     $reduce: {
+				//         input: '$$buffs',
+				//         initialValue: [],
+				//         in: { $concatArrays: ['$$value', '$$this'] },
+				//     },
+				// },
+			])
+			// https://docs.mongodb.com/manual/reference/method/cursor.sort/index.html#sort-asc-desc
+			.sort({
+				rarity: -1,
+				name: 1,
+			});
+
+		return await queryCursorForHeroList
+			.toArray()
+			.then((heroList = []) => {
+				if (heroList.length) {
+					heroList.forEach((hero) => {
+						hero.buffs = [].concat(...hero.buffs);
+						hero.debuffs = [].concat(...hero.debuffs);
+					});
+					return mountApiResponse({}, res, null, heroList);
+				} else {
+					return new Promise.reject();
+				}
+			})
+			.catch(() => {
+				return mountApiErrorResponse(res, MESSAGES.query.invalid);
+			});
 	})
 );
 
