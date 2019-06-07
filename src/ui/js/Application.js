70c4150026af71e4774246bd95f5003212390637
Raphael DDL Oliveira <raphael.s.oliveira@bloomingdales.com>
2019-06-07 15:36:01 -0300

    documentation page - 0.5.0

--- src/ui/js/Application.js
+++ src/ui/js/Application.js
@@ -1,5 +1,48 @@
 (function() {
 	//* ------------------------
-	// Section:
+	// Section: All code examples with Tabs
+	// .tabs followed by .codetabs
+	// each .tabs li a must have a data-results
+	// each div inside .codetabs must have a data-code with same value of data-results of it's tab
 	//------------------------ */
+	const tabs = document.querySelectorAll('.tabs');
+	if (tabs.length) {
+		tabs.forEach((tabList) => {
+			let codeTabs = false;
+			if (tabList.nextElementSibling.classList.contains('codetabs')) {
+				codeTabs = tabList.nextElementSibling;
+			}
+
+			//no codetabs for that set of tabs
+			if (!codeTabs) {
+				return;
+			}
+
+			tabList.querySelectorAll('li').forEach((tabLi) => {
+				tabLi.addEventListener(
+					'click',
+					function(ev) {
+						ev.preventDefault();
+						ev.stopPropagation();
+
+						const { results } = this.querySelector('a').dataset;
+						let codeblock;
+
+						if (results) {
+							codeblock = codeTabs.querySelector(`div[data-code="${results}"]`);
+							if (codeblock) {
+								tabList.querySelectorAll('li').forEach((li) => li.classList.remove('is-active'));
+								tabLi.classList.add('is-active');
+
+								codeTabs
+									.querySelectorAll('div[data-code]')
+									.forEach((div) => div.classList.add('is-hidden'));
+								codeblock.classList.remove('is-hidden');
+							}
+						}
+					}.bind(tabLi)
+				);
+			});
+		});
+	}
 })();
