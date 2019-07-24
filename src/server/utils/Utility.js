9a984e76adbf9095e7496b3f573baa082a1b59d9
Raphael DDL Oliveira <raphael.s.oliveira@bloomingdales.com>
2019-07-24 16:39:58 -0300

    updated shuffleArray to return new array

--- src/server/utils/Utility.js
+++ src/server/utils/Utility.js
@@ -66,10 +66,12 @@ export const cLog = (logLevel = 'log', ...messages) => {
 export function haltOnTimedout(req, res, next) {
 	if (!req.timedout) next();
 }
-// https://stackoverflow.com/a/12646864
+// https://gist.github.com/nikolas/96586a0b56f53eabfd6fe4ed59fecb98
 export function shuffleArray(array) {
-	for (let i = array.length - 1; i > 0; i--) {
+	const a = array.slice();
+	for (let i = a.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
-		[array[i], array[j]] = [array[j], array[i]];
+		[a[i], a[j]] = [a[j], a[i]];
 	}
+	return a;
 }
